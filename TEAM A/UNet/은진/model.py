fbace2ea1062ac9c7ac9ef94805252821a23f0b7
molba2see <molba2see@gmail.com>
2025-04-07 13:36:13 +0900

    [Modify] model.py
    Dropout 추가, 깊이 축소, 출력 활성화 함수 명시

--- TEAM A/UNet/은진/model.py
+++ TEAM A/UNet/은진/model.py
@@ -1,4 +1,4 @@
-# 📁 Step 3: model.py
+# 📁 Step 3: model.py ❤️
 # U-Net 모델 구조 정의
 
 import os
@@ -6,103 +6,64 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-# U-Net 모델 정의
 class UNet(nn.Module):
     def __init__(self):
         super(UNet, self).__init__()
 
-        # Conv + BN + ReLU 블록 생성 함수
-        def CBR2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True):
+        # Conv + BN + ReLU (+ Dropout) 블록 생성 함수
+        def CBR2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, dropout=0.0):
             layers = [
-                nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=bias),
-                nn.BatchNorm2d(out_channels),
-                nn.ReLU()
+                nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=True),  # 컨볼루션
+                nn.BatchNorm2d(out_channels),                                                    # 배치 정규화
+                nn.ReLU(inplace=True)                                                            # ReLU 활성화
             ]
+            if dropout > 0.0: # 드롭아웃 인자 받으면 하게 함
+                layers.append(nn.Dropout2d(dropout))                                             # 드롭아웃 (선택적으로)
             return nn.Sequential(*layers)
 
-        # 인코더 (Contracting path)
-        self.enc1_1 = CBR2d(1, 64)
-        self.enc1_2 = CBR2d(64, 64)
+        # 인코더 블록: 이미지에서 특징 추출
+        self.enc1_1 = CBR2d(1, 32)
+        self.enc1_2 = CBR2d(32, 32)      # 64에서 32로 수정해봄
         self.pool1 = nn.MaxPool2d(2)
 
-        self.enc2_1 = CBR2d(64, 128)
-        self.enc2_2 = CBR2d(128, 128)
+        self.enc2_1 = CBR2d(32, 64)      # 역시 1/2로 줄임
+        self.enc2_2 = CBR2d(64, 64)
         self.pool2 = nn.MaxPool2d(2)
 
-        self.enc3_1 = CBR2d(128, 256)
-        self.enc3_2 = CBR2d(256, 256)
+        self.enc3_1 = CBR2d(64, 128, dropout=0.1)  # 마지막 인코더 블록에 드롭아웃 추가
         self.pool3 = nn.MaxPool2d(2)
 
-        self.enc4_1 = CBR2d(256, 512)
-        self.enc4_2 = CBR2d(512, 512)
-        self.pool4 = nn.MaxPool2d(2)
+        # 디코더 블록: 특징 복원 및 업샘플링
+        self.dec3_1 = CBR2d(128, 64) # 디코더 블록 추가
+        self.unpool2 = nn.ConvTranspose2d(64, 64, kernel_size=2, stride=2)  # 업샘플링
 
-        self.enc5_1 = CBR2d(512, 1024)  # bottleneck
+        self.dec2_1 = CBR2d(128, 32)  # skip 연결 포함: 64(업샘플링) + 64(인코더)
+        self.unpool1 = nn.ConvTranspose2d(32, 32, kernel_size=2, stride=2)
 
-        # 디코더 (Expansive path)
-        self.dec5_1 = CBR2d(1024, 512)
-        self.unpool4 = nn.ConvTranspose2d(512, 512, kernel_size=2, stride=2)
+        self.dec1_1 = CBR2d(64, 32)   # skip 연결 포함: 32(업샘플링) + 32(인코더)
 
-        self.dec4_2 = CBR2d(1024, 512)
-        self.dec4_1 = CBR2d(512, 256)
-        self.unpool3 = nn.ConvTranspose2d(256, 256, kernel_size=2, stride=2)
-
-        self.dec3_2 = CBR2d(512, 256)
-        self.dec3_1 = CBR2d(256, 128)
-        self.unpool2 = nn.ConvTranspose2d(128, 128, kernel_size=2, stride=2)
-
-        self.dec2_2 = CBR2d(256, 128)
-        self.dec2_1 = CBR2d(128, 64)
-        self.unpool1 = nn.ConvTranspose2d(64, 64, kernel_size=2, stride=2)
-
-        self.dec1_2 = CBR2d(128, 64)
-        self.dec1_1 = CBR2d(64, 64)
-
-        # 최종 출력 계층: 채널 수를 1로 줄임 (binary segmentation)
-        self.fc = nn.Conv2d(64, 1, kernel_size=1)
+        # 최종 출력 계층: 채널 수 1로 줄이고 Sigmoid로 이진값 생성
+        self.final_conv = nn.Conv2d(32, 1, kernel_size=1)
+        self.final_activation = nn.Sigmoid()
 
     def forward(self, x):
         # 인코더 경로
-        enc1_1 = self.enc1_1(x)
-        enc1_2 = self.enc1_2(enc1_1)
-        pool1 = self.pool1(enc1_2)
-
-        enc2_1 = self.enc2_1(pool1)
-        enc2_2 = self.enc2_2(enc2_1)
-        pool2 = self.pool2(enc2_2)
-
-        enc3_1 = self.enc3_1(pool2)
-        enc3_2 = self.enc3_2(enc3_1)
-        pool3 = self.pool3(enc3_2)
-
-        enc4_1 = self.enc4_1(pool3)
-        enc4_2 = self.enc4_2(enc4_1)
-        pool4 = self.pool4(enc4_2)
-
-        enc5_1 = self.enc5_1(pool4)
+        enc1 = self.enc1_2(self.enc1_1(x))     # 인코더 블록 1
+        enc2 = self.enc2_2(self.enc2_1(self.pool1(enc1)))  # 인코더 블록 2
+        enc3 = self.enc3_1(self.pool2(enc2))   # 인코더 블록 3
 
         # 디코더 경로
-        dec5_1 = self.dec5_1(enc5_1)
+        dec3 = self.dec3_1(enc3)
+        up2 = self.unpool2(dec3)              # 업샘플링 (2배)
+        cat2 = torch.cat([up2, enc2], dim=1)  # 스킵 연결
+        dec2 = self.dec2_1(cat2)
 
-        unpool4 = self.unpool4(dec5_1)
-        cat4 = torch.cat((unpool4, enc4_2), dim=1)
-        dec4_2 = self.dec4_2(cat4)
-        dec4_1 = self.dec4_1(dec4_2)
+        up1 = self.unpool1(dec2)
+        cat1 = torch.cat([up1, enc1], dim=1)
+        dec1 = self.dec1_1(cat1)
 
-        unpool3 = self.unpool3(dec4_1)
-        cat3 = torch.cat((unpool3, enc3_2), dim=1)
-        dec3_2 = self.dec3_2(cat3)
-        dec3_1 = self.dec3_1(dec3_2)
+        # 최종 출력 (1채널 + Sigmoid로 이진 마스크 생성)
+        out = self.final_conv(dec1)
+        out = self.final_activation(out)
 
-        unpool2 = self.unpool2(dec3_1)
-        cat2 = torch.cat((unpool2, enc2_2), dim=1)
-        dec2_2 = self.dec2_2(cat2)
-        dec2_1 = self.dec2_1(dec2_2)
-
-        unpool1 = self.unpool1(dec2_1)
-        cat1 = torch.cat((unpool1, enc1_2), dim=1)
-        dec1_2 = self.dec1_2(cat1)
-        dec1_1 = self.dec1_1(dec1_2)
-
-        x = self.fc(dec1_1)  # 최종 출력
-        return x
+        return out
