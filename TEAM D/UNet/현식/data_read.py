9126b40dac33751542f8f94b906db39331b23573
조현식 HyeonSik Jo <7johs7@gmail.com>
2025-05-05 23:21:00 +0900

    chore: switch to Cityscapes dataset

--- TEAM D/UNet/현식/data_read.py
+++ TEAM D/UNet/현식/data_read.py
@@ -1,74 +1,21 @@
 import os
-import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 
-dir_data = './datasets'
-name_label = 'train-labels.tif'
-name_input = 'train-volume.tif'
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+device = torch.device(device)
+print(device)
 
-img_label = Image.open(os.path.join(dir_data, name_label))
-img_input = Image.open(os.path.join(dir_data, name_input))
+root_path = './cityscapes_data'
+data_dir = root_path
 
-ny, nx = img_label.size
-nframe = img_label.n_frames
+train_dir = os.path.join(data_dir, "train")
+val_dir = os.path.join(data_dir, "val")
 
-nframe_train = 24
-nframe_val = 3
-nframe_test = 3
+train_fns = os.listdir(train_dir)
+val_fns = os.listdir(val_dir)
 
-dir_save_train = os.path.join(dir_data, 'train')
-dir_save_val = os.path.join(dir_data, 'val')
-dir_save_test = os.path.join(dir_data, 'test')
-
-if not os.path.exists(dir_save_train):
-    os.makedirs(dir_save_train)
-
-if not os.path.exists(dir_save_val):
-    os.makedirs(dir_save_val)
-
-if not os.path.exists(dir_save_test):
-    os.makedirs(dir_save_test)
-
-id_frame = np.arange(nframe)
-np.random.shuffle(id_frame)
-
-offset_nframe = 0
-
-for i in range(nframe_train):
-    img_label.seek(id_frame[i+offset_nframe])
-    img_input.seek(id_frame[i+offset_nframe])
-
-    label_ = np.asarray(img_label)
-    input_ = np.asarray(img_input)
-
-    np.save(os.path.join(dir_save_train, 'label_%03d.npy' % i), label_)
-    np.save(os.path.join(dir_save_train, 'input_%03d.npy' % i), input_)
-
-offset_nframe  += nframe_train
-
-for i in range(nframe_val):
-    img_label.seek(id_frame[i+offset_nframe])
-    img_input.seek(id_frame[i+offset_nframe])
-
-    label_ = np.asarray(img_label)
-    input_ = np.asarray(img_input)
-
-    np.save(os.path.join(dir_save_val, 'label_%03d.npy' % i), label_)
-    np.save(os.path.join(dir_save_val, 'input_%03d.npy' % i), input_)
-
-
-offset_nframe  += nframe_val
-
-for i in range(nframe_test):
-    img_label.seek(id_frame[i+offset_nframe])
-    img_input.seek(id_frame[i+offset_nframe])
-
-    label_ = np.asarray(img_label)
-    input_ = np.asarray(img_input)
-
-    np.save(os.path.join(dir_save_test, 'label_%03d.npy' % i), label_)
-    np.save(os.path.join(dir_save_test, 'input_%03d.npy' % i), input_)
-
-
-
+print(len(train_fns), len(val_fns))
