38844f5a111667e22c55ab4db66cacdb5d6d311b
yeongchaae <yjo515139@gmail.com>
2025-05-06 17:44:47 +0900

    Add files via upload

---
+++ TEAM B/UNet/영채/dataset.py
@@ -0,0 +1,38 @@
+import torch
+import numpy as np
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, data_dir, transform=None):
+        self.data_dir = data_dir
+        self.transform = transform
+        lst_data = os.listdir(self.data_dir)
+        lst_label = [f for f in lst_data if f.startswith('label')]
+        lst_input = [f for f in lst_data if f.startswith('input')]
+        lst_label.sort()
+        lst_input.sort()
+        self.lst_label = lst_label
+        self.lst_input = lst_input
+
+    def __len__(self):
+        return len(self.lst_label)
+
+    def __getitem__(self, index):
+        label = np.load(os.path.join(self.data_dir, self.lst_label[index]))
+        input = np.load(os.path.join(self.data_dir, self.lst_input[index]))
+        label = label / 255.0
+        input = input / 255.0
+        if label.ndim == 2:
+            label = label[:, :, np.newaxis]
+        if input.ndim == 2:
+            input = input[:, :, np.newaxis]
+        data = {'input': input, 'label': label}
+        if self.transform:
+            data = self.transform(data)
+        return data
+
+class ToTensor(object):
+    def __call__(self, data):
+        label, input = data['label'], data['input']
+        label = label.transpose((2, 0, 1)).astype(np.float32)
+        input = input.transpose((2, 0, 1)).astype(np.float32)
+        return {'label': torch.from_numpy(label), 'input': torch.from_numpy(input)}
