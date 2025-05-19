39941a1c2486ec020594b9d4838535bbfc9b5bcb
stem-sw <133653337+stem-sw@users.noreply.github.com>
2025-05-19 19:47:06 +0900

    Add files via upload

---
+++ TEAM A/Transformer/성원/models/layers/position_wise_feed_forward.py
@@ -0,0 +1,24 @@
+""" 
+Transformer 블록 안에서 self-attention 이후 각 위치별로 독립적으로 작동하는 두 개의 fully connected layer를 구현
+"""
+
+
+
+from torch import nn
+
+
+class PositionwiseFeedForward(nn.Module):
+
+    def __init__(self, d_model, hidden, drop_prob=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.linear1 = nn.Linear(d_model, hidden)
+        self.linear2 = nn.Linear(hidden, d_model)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=drop_prob)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.linear2(x)
+        return x
