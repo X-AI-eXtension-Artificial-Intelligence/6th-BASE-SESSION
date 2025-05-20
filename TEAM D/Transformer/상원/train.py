d946df712c872085f953df6a04585bd80e8bec98
leesangwon393 <lee990924@kookmin.ac.kr>
2025-05-20 18:44:14 +0900

    new_transformer

--- TEAM D/Transformer/상원/train.py
+++ TEAM D/Transformer/상원/train.py
@@ -1,161 +1,274 @@
-"""
-@author : Hyunwoong
-@when : 2019-10-22
-@homepage : https://github.com/gusdnd852
-"""
-import math
-import time
+from model import build_transformer
+from dataset import BilingualDataset, causal_mask
+from config import get_config, get_weights_file_path, latest_weights_file_path
 
-from torch import nn, optim     # 신경망 계층(nn)과 최적화 함수(optim) 불러오기
-from torch.optim import Adam    # Adam 옵티마이저 불러오기
+import torchtext.datasets as datasets
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader, random_split
+from torch.optim.lr_scheduler import LambdaLR
 
-from data import *
-from models.model.transformer import Transformer    # Transformer 모델 클래스 불러오기기
-from util.bleu import idx_to_word, get_bleu         # idx_to_word: 인덱스를 실제 단어로 변환하는 함수 , get_bleu: 모델의 번역 성능 평가를 위한 BLEU 점수 계산 함수
-from util.epoch_timer import epoch_time         
+import warnings
+from tqdm import tqdm
+import os
+from pathlib import Path
 
-# 모델의 학습 가능한(gradient를 계산하는) 전체 파라미터 수를 반환
-def count_parameters(model):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)    # p.numel(): 각 파라미터 텐서의 요소 수 , requires_grad: 학습 가능한 파라미터만 선택
+# Huggingface datasets and tokenizers
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
 
-# 주어진 모듈 m에 대해 He(kaiming) 초기화를 적용
-def initialize_weights(m):
-    if hasattr(m, 'weight') and m.weight.dim() > 1:     # weight의 차원이 2 이상이면 초기화
-        nn.init.kaiming_uniform(m.weight.data)
+import torchmetrics
+from torch.utils.tensorboard import SummaryWriter
+
+def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device):
+    sos_idx = tokenizer_tgt.token_to_id('[SOS]')
+    eos_idx = tokenizer_tgt.token_to_id('[EOS]')
+
+    # Precompute the encoder output and reuse it for every step
+    encoder_output = model.encode(source, source_mask)
+    # Initialize the decoder input with the sos token
+    decoder_input = torch.empty(1, 1).fill_(sos_idx).type_as(source).to(device)
+    while True:
+        if decoder_input.size(1) == max_len:
+            break
+
+        # build mask for target
+        decoder_mask = causal_mask(decoder_input.size(1)).type_as(source_mask).to(device)
+
+        # calculate output
+        out = model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
+
+        # get next token
+        prob = model.project(out[:, -1])
+        _, next_word = torch.max(prob, dim=1)
+        decoder_input = torch.cat(
+            [decoder_input, torch.empty(1, 1).type_as(source).fill_(next_word.item()).to(device)], dim=1
+        )
+
+        if next_word == eos_idx:
+            break
+
+    return decoder_input.squeeze(0)
 
 
-model = Transformer(src_pad_idx=src_pad_idx,        # 소스 문장에서의 패딩 토큰 인덱스
-                    trg_pad_idx=trg_pad_idx,        # 타겟 문장에서의 패딩 토큰 인덱스
-                    trg_sos_idx=trg_sos_idx,        # 타겟 문장에서의 시작 토큰 인덱스
-                    d_model=d_model,                # 임베딩 차원 (예: 512) → 전체 모델의 feature 크기
-                    enc_voc_size=enc_voc_size,      # 인코더의 전체 단어 집합 크기
-                    dec_voc_size=dec_voc_size,      # 디코더의 전체 단어 집합 크기
-                    max_len=max_len,                # 최대 시퀀스 길이
-                    ffn_hidden=ffn_hidden,          # 피드포워드 신경망(hidden layer)의 크기
-                    n_head=n_heads,                 # 멀티헤드 어텐션의 head 수
-                    n_layers=n_layers,              # 인코더/디코더 블록의 레이어 수
-                    drop_prob=drop_prob,            # 드롭아웃 
-                    device=device).to(device)       # 모델을 지정한 디바이스로 이동
-
-print(f'The model has {count_parameters(model):,} trainable parameters')
-model.apply(initialize_weights)                                         # 모델 가중치 초기화
-# 옵티마이저 설정 (Adam)
-optimizer = Adam(params=model.parameters(),         # 학습할 파라미터 (모델의 모든 가중치)
-                 lr=init_lr,                        # 초기 학습률 
-                 weight_decay=weight_decay,         # 가중치 감쇠(L2 정규화) 계수
-                 eps=adam_eps)                      # 작은 값으로 분모가 0 되는 걸 방지
-# 스케줄러 설정 (ReduceLROnPlateau)
-scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer,
-                                                 verbose=True,          # 학습률이 줄어들 때마다 로그 출력
-                                                 factor=factor,         # 감소 계수
-                                                 patience=patience)
-# 손실 함수 정의 (CrossEntropyLoss)
-criterion = nn.CrossEntropyLoss(ignore_index=src_pad_idx)               # 패딩 토큰 인덱스는 손실 계산에서 무시
-
-# 학습
-def train(model, iterator, optimizer, criterion, clip):
-    model.train()
-    epoch_loss = 0
-    for i, batch in enumerate(iterator):
-        src = batch.src     # 소스 시퀀스 (입력)
-        trg = batch.trg     # 타겟 시퀀스 (정답)
-
-        
-        optimizer.zero_grad()                   # 이전 스텝의 gradient 초기화
-        output = model(src, trg[:, :-1])        # 모델 forward
-        output_reshape = output.contiguous().view(-1, output.shape[-1]) # 크로스엔트로피는 2D 입력 필요 → (batch_size * trg_len-1, output_dim)로 reshape
-        trg = trg[:, 1:].contiguous().view(-1)                          # 타겟도 (batch_size * trg_len-1)로 평탄화
-
-        # 손실 계산
-        loss = criterion(output_reshape, trg)
-        # 역전파
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)        # gradient clipping: gradient 폭주 방지
-        # 파라미터 업데이트
-        optimizer.step()
-
-        epoch_loss += loss.item()
-        print('step :', round((i / len(iterator)) * 100, 2), '% , loss :', loss.item())
-    # 전체 에폭 동안의 평균 손실 반환
-    return epoch_loss / len(iterator)
-
-# 평가
-def evaluate(model, iterator, criterion):
+def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, device, print_msg, global_step, writer, num_examples=2):
     model.eval()
-    epoch_loss = 0
-    batch_bleu = []
-    with torch.no_grad():           # 평가 중에는 gradient를 계산하지 않음
-        for i, batch in enumerate(iterator):
-            src = batch.src
-            trg = batch.trg
-            output = model(src, trg[:, :-1])
-            output_reshape = output.contiguous().view(-1, output.shape[-1])
-            trg = trg[:, 1:].contiguous().view(-1)
+    count = 0
 
-            loss = criterion(output_reshape, trg)
-            epoch_loss += loss.item()
+    source_texts = []
+    expected = []
+    predicted = []
+
+    try:
+        # get the console window width
+        with os.popen('stty size', 'r') as console:
+            _, console_width = console.read().split()
+            console_width = int(console_width)
+    except:
+        # If we can't get the console width, use 80 as default
+        console_width = 80
+
+    with torch.no_grad():
+        for batch in validation_ds:
+            count += 1
+            encoder_input = batch["encoder_input"].to(device) # (b, seq_len)
+            encoder_mask = batch["encoder_mask"].to(device) # (b, 1, 1, seq_len)
+
+            # check that the batch size is 1
+            assert encoder_input.size(
+                0) == 1, "Batch size must be 1 for validation"
+
+            model_out = greedy_decode(model, encoder_input, encoder_mask, tokenizer_src, tokenizer_tgt, max_len, device)
+
+            source_text = batch["src_text"][0]
+            target_text = batch["tgt_text"][0]
+            model_out_text = tokenizer_tgt.decode(model_out.detach().cpu().numpy())
+
+            source_texts.append(source_text)
+            expected.append(target_text)
+            predicted.append(model_out_text)
             
-            # BLEU 점수 계산
-            total_bleu = []
-            for j in range(batch_size):
-                # 타겟 문장을 인덱스 → 단어로 변환
-                try:
-                    trg_words = idx_to_word(batch.trg[j], loader.target.vocab)
-                    # 출력: 가장 높은 확률의 단어 인덱스 추출
-                    output_words = output[j].max(dim=1)[1]
-                    output_words = idx_to_word(output_words, loader.target.vocab)
-                    # BLEU 점수 계산
-                    bleu = get_bleu(hypotheses=output_words.split(), reference=trg_words.split())
-                    total_bleu.append(bleu)
-                except:
-                    pass
-            # 배치 BLEU 점수 평균
-            total_bleu = sum(total_bleu) / len(total_bleu)
-            batch_bleu.append(total_bleu)
-    # 전체 배치의 평균 BLEU 점수
-    batch_bleu = sum(batch_bleu) / len(batch_bleu)
-    return epoch_loss / len(iterator), batch_bleu
+            # Print the source, target and model output
+            print_msg('-'*console_width)
+            print_msg(f"{f'SOURCE: ':>12}{source_text}")
+            print_msg(f"{f'TARGET: ':>12}{target_text}")
+            print_msg(f"{f'PREDICTED: ':>12}{model_out_text}")
 
-# 학습 및 평가 루프를 실행
-def run(total_epoch, best_loss):
-    train_losses, test_losses, bleus = [], [], []       # 손실 및 BLEU 기록용 리스트
-    for step in range(total_epoch):
-        # 학습 루프 실행
-        start_time = time.time()
-        train_loss = train(model, train_iter, optimizer, criterion, clip)
-        # 검증 루프 실행
-        valid_loss, bleu = evaluate(model, valid_iter, criterion)
-        end_time = time.time()
+            if count == num_examples:
+                print_msg('-'*console_width)
+                break
+    
+    if writer:
+        # Evaluate the character error rate
+        # Compute the char error rate 
+        metric = torchmetrics.CharErrorRate()
+        cer = metric(predicted, expected)
+        writer.add_scalar('validation cer', cer, global_step)
+        writer.flush()
 
-        if step > warmup:
-            scheduler.step(valid_loss)
+        # Compute the word error rate
+        metric = torchmetrics.WordErrorRate()
+        wer = metric(predicted, expected)
+        writer.add_scalar('validation wer', wer, global_step)
+        writer.flush()
 
-        train_losses.append(train_loss)
-        test_losses.append(valid_loss)
-        bleus.append(bleu)
-        epoch_mins, epoch_secs = epoch_time(start_time, end_time)
-        #  모델 저장 (최고 성능 모델)
-        if valid_loss < best_loss:
-            best_loss = valid_loss
-            torch.save(model.state_dict(), 'saved/model-{0}.pt'.format(valid_loss))
+        # Compute the BLEU metric
+        metric = torchmetrics.BLEUScore()
+        bleu = metric(predicted, expected)
+        writer.add_scalar('validation BLEU', bleu, global_step)
+        writer.flush()
 
-        f = open('result/train_loss.txt', 'w')
-        f.write(str(train_losses))
-        f.close()
+def get_all_sentences(ds, lang):
+    for item in ds:
+        yield item['translation'][lang]
 
-        f = open('result/bleu.txt', 'w')
-        f.write(str(bleus))
-        f.close()
+def get_or_build_tokenizer(config, ds, lang):
+    tokenizer_path = Path(config['tokenizer_file'].format(lang))
+    if not Path.exists(tokenizer_path):
+        # Most code taken from: https://huggingface.co/docs/tokenizers/quicktour
+        tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
+        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+    return tokenizer
 
-        f = open('result/test_loss.txt', 'w')
-        f.write(str(test_losses))
-        f.close()
+def get_ds(config):
+    # It only has the train split, so we divide it overselves
+    ds_raw = load_dataset(f"{config['datasource']}", f"{config['lang_src']}-{config['lang_tgt']}", split='train')
 
-        print(f'Epoch: {step + 1} | Time: {epoch_mins}m {epoch_secs}s')
-        print(f'\tTrain Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f}')
-        print(f'\tVal Loss: {valid_loss:.3f} |  Val PPL: {math.exp(valid_loss):7.3f}')
-        print(f'\tBLEU Score: {bleu:.3f}')
+    # Build tokenizers
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
+    tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
+
+    # Keep 90% for training, 10% for validation
+    train_ds_size = int(0.9 * len(ds_raw))
+    val_ds_size = len(ds_raw) - train_ds_size
+    train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
+
+    train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+
+    # Find the maximum length of each sentence in the source and target sentence
+    max_len_src = 0
+    max_len_tgt = 0
+
+    for item in ds_raw:
+        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+        tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_tgt']]).ids
+        max_len_src = max(max_len_src, len(src_ids))
+        max_len_tgt = max(max_len_tgt, len(tgt_ids))
+
+    print(f'Max length of source sentence: {max_len_src}')
+    print(f'Max length of target sentence: {max_len_tgt}')
+    
+
+    train_dataloader = DataLoader(train_ds, batch_size=config['batch_size'], shuffle=True)
+    val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
+
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
+
+def get_model(config, vocab_src_len, vocab_tgt_len):
+    model = build_transformer(vocab_src_len, vocab_tgt_len, config["seq_len"], config['seq_len'], d_model=config['d_model'])
+    return model
+
+def train_model(config):
+    # Define the device
+    device = "cuda" if torch.cuda.is_available() else "mps" if torch.has_mps or torch.backends.mps.is_available() else "cpu"
+    print("Using device:", device)
+    if (device == 'cuda'):
+        print(f"Device name: {torch.cuda.get_device_name(device.index)}")
+        print(f"Device memory: {torch.cuda.get_device_properties(device.index).total_memory / 1024 ** 3} GB")
+    elif (device == 'mps'):
+        print(f"Device name: <mps>")
+    else:
+        print("NOTE: If you have a GPU, consider using it for training.")
+        print("      On a Windows machine with NVidia GPU, check this video: https://www.youtube.com/watch?v=GMSjDTU8Zlc")
+        print("      On a Mac machine, run: pip3 install --pre torch torchvision torchaudio torchtext --index-url https://download.pytorch.org/whl/nightly/cpu")
+    device = torch.device(device)
+
+    # Make sure the weights folder exists
+    Path(f"{config['datasource']}_{config['model_folder']}").mkdir(parents=True, exist_ok=True)
+
+    train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(config)
+    model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size()).to(device)
+    # Tensorboard
+    writer = SummaryWriter(config['experiment_name'])
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'], eps=1e-9)
+
+    # If the user specified a model to preload before training, load it
+    initial_epoch = 0
+    global_step = 0
+    preload = config['preload']
+    model_filename = latest_weights_file_path(config) if preload == 'latest' else get_weights_file_path(config, preload) if preload else None
+    if model_filename:
+        print(f'Preloading model {model_filename}')
+        state = torch.load(model_filename)
+        model.load_state_dict(state['model_state_dict'])
+        initial_epoch = state['epoch'] + 1
+        optimizer.load_state_dict(state['optimizer_state_dict'])
+        global_step = state['global_step']
+    else:
+        print('No model to preload, starting from scratch')
+
+    loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id('[PAD]'), label_smoothing=0.1).to(device)
+
+    for epoch in range(initial_epoch, config['num_epochs']):
+        torch.cuda.empty_cache()
+        model.train()
+        batch_iterator = tqdm(train_dataloader, desc=f"Processing Epoch {epoch:02d}")
+        for batch in batch_iterator:
+
+            encoder_input = batch['encoder_input'].to(device) # (b, seq_len)
+            decoder_input = batch['decoder_input'].to(device) # (B, seq_len)
+            encoder_mask = batch['encoder_mask'].to(device) # (B, 1, 1, seq_len)
+            decoder_mask = batch['decoder_mask'].to(device) # (B, 1, seq_len, seq_len)
+
+            # Run the tensors through the encoder, decoder and the projection layer
+            encoder_output = model.encode(encoder_input, encoder_mask) # (B, seq_len, d_model)
+            decoder_output = model.decode(encoder_output, encoder_mask, decoder_input, decoder_mask) # (B, seq_len, d_model)
+            proj_output = model.project(decoder_output) # (B, seq_len, vocab_size)
+
+            # Compare the output with the label
+            label = batch['label'].to(device) # (B, seq_len)
+
+            # Compute the loss using a simple cross entropy
+            loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1))
+            batch_iterator.set_postfix({"loss": f"{loss.item():6.3f}"})
+
+            # Log the loss
+            writer.add_scalar('train loss', loss.item(), global_step)
+            writer.flush()
+
+            # Backpropagate the loss
+            loss.backward()
+
+            # Update the weights
+            optimizer.step()
+            optimizer.zero_grad(set_to_none=True)
+
+            global_step += 1
+
+        # Run validation at the end of every epoch
+        run_validation(model, val_dataloader, tokenizer_src, tokenizer_tgt, config['seq_len'], device, lambda msg: batch_iterator.write(msg), global_step, writer)
+
+        # Save the model at the end of every epoch
+        model_filename = get_weights_file_path(config, f"{epoch:02d}")
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'global_step': global_step
+        }, model_filename)
 
 
 if __name__ == '__main__':
-    run(total_epoch=epoch, best_loss=inf)
+    warnings.filterwarnings("ignore")
+    config = get_config()
+    train_model(config)
